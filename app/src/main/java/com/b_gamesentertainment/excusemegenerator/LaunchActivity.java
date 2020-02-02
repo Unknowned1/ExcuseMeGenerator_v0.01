0910c14f56959d0381416bb33f0d4e61b70de903
AlexBacco <a.bacco@outlook.com>
2020-02-03 00:57:40 +0100

    remade_2_1_5

---
+++ app/src/main/java/com/b_gamesentertainment/excusemegenerator/LaunchActivity.java
@@ -0,0 +1,46 @@
+package com.b_gamesentertainment.excusemegenerator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import static com.b_gamesentertainment.excusemegenerator.R.anim.rotate;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    ImageView img_view;
+    private static int TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_activity);
+
+        img_view = findViewById(R.id.startingImage);
+        RotateAnimation rotate = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        rotate.setDuration(2000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        img_view.setAnimation(rotate);
+
+        /*
+        Intent intent = new Intent(this, MainActivity.class );
+        startActivity(intent);
+        */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class );
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+
+    }
+}
--- app/src/main/java/com/b_gamesentertainment/excusemegenerator/MainActivity.java
+++ app/src/main/java/com/b_gamesentertainment/excusemegenerator/MainActivity.java
@@ -18,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
     TextView textViewScuse;
 
     String[] arrayJob = {"Stringa1", "Stringa2", "Stringa3", "Stringa4", "Stringa5", "Stringa6", "Stringa7"};
-
-    //String[] array1 = {"Stringa8", "Stringa9", "Stringa10", "Stringa11", "Stringa12", "Stringa13", "Stringa14"};
-
     ArrayAdapter<String> adapter;
 
 
@@ -39,30 +36,16 @@ public class MainActivity extends AppCompatActivity {
         buttonLovers = findViewById(R.id.buttonLovers);
 
         textViewScuse = findViewById(R.id.textViewScuse);
-        textViewScuse.setText(arrayJob[0]);
+        textViewScuse.setText("ciao");
 
-
-
-
-
-
-      /*  listaScuse = findViewById(R.id.listaScuse);
-        adapter = new ArrayAdapter<String>(this,R.layout.list_element,
-                R.id.textViewList, array );
-        */
-        //listaScuse.setAdapter(adapter);
 
         buttonJob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Random random = new Random();
-
-                int indice = random.nextInt(arrayJob.length +1 );
-
+                int indice = random.nextInt(arrayJob.length );
                 textViewScuse.setText(arrayJob[indice]);
-
-
             }
         });
 
