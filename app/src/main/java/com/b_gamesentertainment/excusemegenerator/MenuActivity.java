03027c9883bf5df4bec4838231b7f8f1aa817dad
AlexBacco <a.bacco@outlook.com>
2020-02-03 18:15:16 +0100

    Aggiunta funzionalità start da menu

--- app/src/main/java/com/b_gamesentertainment/excusemegenerator/MenuActivity.java
+++ app/src/main/java/com/b_gamesentertainment/excusemegenerator/MenuActivity.java
@@ -1,16 +1,35 @@
 package com.b_gamesentertainment.excusemegenerator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button startButton, settingsButton, exitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
 
+        startButton = findViewById(R.id.start_button);
+        settingsButton = findViewById(R.id.settings_button);
+        exitButton = findViewById(R.id.exit_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class );
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
